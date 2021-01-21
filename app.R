5a833414cbf8f804d08f20973e268c96544be5e9
Ricky Soo <8564646+rickysoo@users.noreply.github.com>
2021-01-21 23:45:16 +0800

    Add files via upload

--- app.R
+++ app.R
@@ -9,32 +9,20 @@ library(stringr)
 # library(magick)
 library(dplyr)
 library(ggplot2)
+library(syuzhet)
 
 # shapes <- sort(c('circle', 'cardioid', 'diamond', 'triangle-forward', 'triangle', 'pentagon', 'star'))
 
 ui <- fluidPage(
     theme = shinytheme('united'),
     titlePanel('Speech Cloud'),
-    p('Make a word cloud for your speech.'),
-    hr(),
+    p('Make a word cloud and analyze your speech.'),
+    # hr(),
     
     sidebarLayout(
         sidebarPanel(
-            selectInput(
-                'speech',
-                label = 'See an example',
-                choices = c(
-                        'An Unbelievable Story - Aaron Beverly' = 'aaron_beverly.txt',
-                        'Changed by a Tyre - Pres Vasilev' = 'pres_vasilev.txt',
-                        'I See Something - Danajaya Hettiarachchi' = 'dananjaya_hettiarachchi.txt',
-                        'Outsmart, Outlast - Darren Tay' = 'darren_tay.txt',
-                        'Pull Less, Bend More - Manoj Vasudevan' = 'manoj_vasudevan.txt',
-                        'Still Standing - Ramona Smith' = 'ramona_smith.txt',
-                        'The Power of Words - Mohammed Qahtani' = 'mohammed_qahtani.txt'
-                    ),
-                selected = 'dananjaya_hettiarachchi.txt'
-            ),
-            
+            uiOutput('speeches'),
+
             textAreaInput('own', 'Paste your speech here', rows = 7),
             
             sliderInput('num', 'Use number of words', min = 10, max = 300, value = 100),
@@ -55,7 +43,10 @@ ui <- fluidPage(
         mainPanel(
             tabsetPanel(
                 tabPanel(
-                    'Cloud',
+                    'Word Cloud',
+                    
+                    br(),
+                    p('The word cloud shows the most spoken words in the speech. The bigger a word is, the more frequently it is used.'),
                     wordcloud2Output('cloud', width = '100%'),
                     
                     tags$head(
@@ -63,15 +54,40 @@ ui <- fluidPage(
                         tags$style(HTML('#regenerate {background-color:#004165}'))
                     ),
                     
-                    hr(),
+                    br(),
                     fluidRow(
-                        column(12, div(actionButton('regenerate', 'Try Another One'), style = "text-align: center"))
+                        column(12, div(actionButton('regenerate', 'Get Another One'), style = "text-align: center"))
                         # column(6, div(downloadButton('download', 'Download'), style = "text-align: left")))
                     )
                 ),
                 tabPanel(
-                    'Words',
+                    'Word List',
+                    
+                    br(),
+                    p('Here are the top 10 words used in the speech. Do you see any pattern?'),
                     plotOutput('words')                    
+                ),
+                tabPanel(
+                    'Sentiments',
+                    
+                    br(),
+                    p('Sentiment analysis shows the positivity and negativity in the speech. A value above 0 is positive. A value below 0 is negative.'),
+                    br(),
+                    
+                    plotOutput('sentiments_plot'),
+                    hr(),
+                    
+                    h3('Break It Down to Sentences'),
+                    tableOutput('sentiments_table')
+                ),
+                tabPanel(
+                    'Watch Video',
+                    
+                    h3(textOutput('video_title')),
+                    tags$style(HTML('.video-container {position: relative; width: 100%; padding-bottom: 56.25%;}')),
+                    tags$style(HTML('.video {position: absolute; top: 0; left: 0; width: 100%; height: 100%; border: 0;}')),
+                    htmlOutput('video'),
+                    a('Video source: Toastmasters World Champions', href = 'https://www.youtube.com/watch?v=7Tev43VNRIc&list=PLZfLuUohfwTYDFDiFFFIl47hyPYe7Z6Xi', target = '_blank')
                 )
             )
         )
@@ -81,35 +97,22 @@ ui <- fluidPage(
 server <- function(input, output, session) {
     values <- reactiveValues(
         source = 'speech',
+        # corpus = NULL,
         df = data.frame()
     )   
-
-    # create_wordcloud <- function(data, num_words = 100, background = 'white', shape = 'circle') {
-    #     if(is.character(data)) {
-    #         corpus <- Corpus(VectorSource(data))
-    #         corpus <- tm_map(corpus, tolower)
-    #         corpus <- tm_map(corpus, removePunctuation)
-    #         corpus <- tm_map(corpus, removeNumbers)
-    #         corpus <- tm_map(corpus, removeWords, stopwords('english'))
-    #         
-    #         tdm <- as.matrix(TermDocumentMatrix(corpus))
-    #         data <- sort(rowSums(tdm), decreasing = TRUE)
-    #         df <- data.frame(word = names(data), freq = as.numeric(data))
-    #     }
-    #     
-    #     if (!is.numeric(num_words) || num_words < 3) {
-    #         num_words <- 3
-    #     }
-    #     
-    #     values$df <- head(df, n = num_words)
-    # 
-    #     if (nrow(values$df) == 0) {
-    #         return(NULL)
-    #     }
-    #     
-    #     wordcloud2(data = values$df, backgroundColor = background, shape = shape, size = 0.6, shuffle = TRUE)
-    # }
-
+    
+    load_speeches <- reactive({
+        read.csv('Speeches.csv') %>%
+            arrange(Title)
+    })
+    
+    get_video_info <- function(file, col) {
+        speeches <- load_speeches() %>%
+            filter(File == file)
+        
+        speeches[[col]]
+    }
+    
     load_df <- reactive({
         if (values$source == 'speech') {
             data <- readLines(input$speech)
@@ -118,7 +121,7 @@ server <- function(input, output, session) {
             data <- input$own
         }
         else {
-            return()
+            return(NULL)
         }
         
         if(is.character(data)) {
@@ -127,79 +130,60 @@ server <- function(input, output, session) {
             corpus <- tm_map(corpus, removePunctuation)
             corpus <- tm_map(corpus, removeNumbers)
             corpus <- tm_map(corpus, removeWords, stopwords('english'))
+            corpus <- tm_map(corpus, stripWhitespace)
+            
+            # corpus_frame <- data.frame(text=unlist(sapply(corpus, `[`, "content")), stringsAsFactors=F)
+            # values$corpus <- corpus
+            # print(corpus)
+            # View(corpus_frame)
             
             tdm <- as.matrix(TermDocumentMatrix(corpus))
             data <- sort(rowSums(tdm), decreasing = TRUE)
             df <- data.frame(word = names(data), freq = as.numeric(data))
         }
         
-        if (!is.numeric(input$num) || input$num < 3) {
-            num_words <- 3
-        }
-        
         values$df <- head(df, n = input$num)
-        
-        if (nrow(values$df) == 0) {
-            return(NULL)
-        }
-        
         values$df
     })  
     
     generate_wordcloud <- reactive({
-        # if (values$source == 'speech') {
-        #     data <- readLines(input$speech)
-        # }
-        # else if (values$source == 'own') {
-        #     data <- input$own
-        # }
-        # else {
-        #     return()
-        # }
-        # 
-        # if(is.character(data)) {
-        #     corpus <- Corpus(VectorSource(data))
-        #     corpus <- tm_map(corpus, tolower)
-        #     corpus <- tm_map(corpus, removePunctuation)
-        #     corpus <- tm_map(corpus, removeNumbers)
-        #     corpus <- tm_map(corpus, removeWords, stopwords('english'))
-        #     
-        #     tdm <- as.matrix(TermDocumentMatrix(corpus))
-        #     data <- sort(rowSums(tdm), decreasing = TRUE)
-        #     df <- data.frame(word = names(data), freq = as.numeric(data))
-        # }
-        # 
-        # if (!is.numeric(input$num) || input$num < 3) {
-        #     num_words <- 3
-        # }
-        # 
-        # values$df <- head(df, n = input$num)
-        # 
-        # if (nrow(values$df) == 0) {
-        #     return(NULL)
-        # }
+        df <- load_df()
         
-        wordcloud2(data = load_df(), backgroundColor = input$col, shape = 'circle', size = 0.6, shuffle = TRUE)
+        if (nrow(df) == 0) {
+            return(NULL)
+        }
         
-        # create_wordcloud(
-        #     data,
-        #     num_words = input$num,
-        #     background = input$col
-        # )
+        wordcloud2(data = df, backgroundColor = input$col, shape = 'circle', size = 0.6, shuffle = TRUE)
+    })
+    
+    output$speeches <- renderUI({
+        speeches <- load_speeches()
+        
+        selectInput(
+            inputId = 'speech',
+            label = 'See an example',
+            choices = setNames(speeches$File, paste0(speeches$Title, ' - ', speeches$Speaker)),
+            selected = 'dananjaya_hettiarachchi.txt'
+        )
     })
     
     output$cloud <- renderWordcloud2({
-        if (input$speech == '') {
-            return()
+        if (is.null(input$speech) | is.null(values$df)) {
+            return(NULL)
         }
-        print('rendering...')
+        
+        print('Rendering word cloud...')
         generate_wordcloud()
     })
     
     output$words <- renderPlot({
         df <- values$df %>%
             head(10)
-
+        
+        if (nrow(df) == 0) {
+            return(NULL)
+        }
+        
         ggplot(data = df, aes(x = reorder(word, freq), y = freq, fill = word)) +
             geom_bar(stat = 'identity') +
             scale_y_continuous(breaks = 0:max(df$freq)) +
@@ -209,17 +193,108 @@ server <- function(input, output, session) {
                 x = 'Words',
                 y = 'Count'
             ) +
+            theme_minimal() +
             theme(
                 plot.title = element_text(face = 'bold', size = 14, hjust = 0.5),
                 axis.title = element_text(face = 'bold'),
                 plot.background = element_blank(),
                 panel.background = element_blank(),
-                # panel.grid.major.y = element_line(color = 'grey'),
                 panel.grid.major.y = element_blank(),
                 legend.position = 'None'
             )
     })
+    
+    extract_sentences <- reactive({
+        if (values$source == 'speech') {
+            text <- readChar(input$speech, file.info(input$speech)$size)
+        }
+        else if (values$source == 'own') {
+            text <- input$own
+        }
         
+        get_sentences(text)
+    })
+
+    extract_sentiments <- reactive({
+        sentences <- extract_sentences()
+        sentiments <- get_sentiment(sentences, method = 'syuzhet')
+        
+        data.frame(
+            'ID' = 1:length(sentences),
+            'Sentence' = sentences,
+            'Sentiment' = sentiments
+        )
+    })    
+    
+    output$sentiments_plot <- renderPlot({
+        df <- extract_sentiments()
+        speech_sentiment <- round(mean(df$Sentiment), 2)
+        print(speech_sentiment)
+        
+        ggplot(data = df, aes(x = ID, y = Sentiment)) +
+            # geom_line(aes(color = '#CD202C')) +
+            # geom_line(aes(color = '#F2DF74')) +
+            geom_line(aes(color = 'blue')) +
+            geom_hline(yintercept = 0, colour = 'gray', size = 1) +
+            labs(
+                title = 'Change of Sentiments in Speech',
+                x = 'Sentences',
+                y = 'Sentiment'
+            ) +
+            annotate(
+                'text', 
+                label = paste0('Overall Sentiment = ', speech_sentiment), 
+                x = 1,
+                y = max(df$Sentiment),
+                color = '#004165',
+                size = 5,
+                hjust = 0
+            ) +
+            theme_minimal() +
+            theme(
+                plot.title = element_text(face = 'bold', size = 14, hjust = 0.5),
+                axis.title = element_text(face = 'bold'),
+                plot.background = element_blank(),
+                panel.background = element_blank(),
+                panel.grid.major.y = element_blank(),
+                legend.position = 'None'
+            )
+    })
+    
+    output$sentiments_table <- renderTable(
+        {
+            extract_sentiments()
+        },
+        
+        striped = TRUE,
+        hover = TRUE,
+        bordered = TRUE,
+        colnames = TRUE,
+        rownames = FALSE
+    )
+    
+    output$video_title <- renderText({
+        if (is.null(input$speech) | values$source != 'speech') {
+            return('No video for your speech')
+        }
+        
+        title <- get_video_info(input$speech, 'Title')
+        speaker <- get_video_info(input$speech, 'Speaker')
+        
+        paste0(title, ' - ', speaker)
+    })
+    
+    output$video <- renderText({
+        if (is.null(input$speech) | values$source != 'speech') {
+            return(NULL)
+        }
+        
+        video <- get_video_info(input$speech, 'Video')
+        
+        HTML(paste0('<div class="video-container"><iframe class="video" src="https://www.youtube.com/embed/', video, '" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>'))
+        # HTML(paste0('<iframe width="560" height="315" src="https://www.youtube.com/embed/', video, '" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'))
+    })
+    
     observeEvent(input$own, {
         values$source <- 'own'
         values$df <- load_df()
@@ -229,7 +304,7 @@ server <- function(input, output, session) {
         values$source <- 'speech'
         values$df <- load_df()
     })
-
+    
     observeEvent(input$regenerate, {
         temp <- values$source
         values$source <- ''
@@ -237,7 +312,7 @@ server <- function(input, output, session) {
         
         generate_wordcloud()
     })
-
+    
     # output$download <- downloadHandler(
     #     filename = 'wordcloud.png',
     #     
